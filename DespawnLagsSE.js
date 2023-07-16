39e76809d5729fd8d5ef73ee1360d8011f318919
minimouse0 <473147334@qq.com>
2023-07-16 20:47:42 +0800

    这缩进疯了？

--- DespawnLagsSE.js
+++ DespawnLagsSE.js
@@ -7,23 +7,23 @@ log("DespawnLagsSE开始运行");
 conf = new JsonConfigFile("plugins/" + PLUGIN_NAME + "/config.json");
 
 conf.init("Config", {
-	DespawnTicks: 600,
-	Whitelist: [
-		"minecraft:netherite_helmet",
-		"minecraft:netherite_sword",
-		"minecraft:netherite_chestplate",
-		"minecraft:netherite_leggings",
-		"minecraft:undyed_shulker_box",
-		"minecraft:netherite_boots",
-		"minecraft:shulker_box",
-		"minecraft:elytra",
-		"minecraft:dragon_egg",
-		"minecraft:nether_star",
-	],
+    DespawnTicks: 600,
+    Whitelist: [
+        "minecraft:netherite_helmet",
+        "minecraft:netherite_sword",
+        "minecraft:netherite_chestplate",
+        "minecraft:netherite_leggings",
+        "minecraft:undyed_shulker_box",
+        "minecraft:netherite_boots",
+        "minecraft:shulker_box",
+        "minecraft:elytra",
+        "minecraft:dragon_egg",
+        "minecraft:nether_star",
+    ],
 });
 
 function getDespawnTime() {
-	return conf.get("Config").DespawnTicks * 50;
+    return conf.get("Config").DespawnTicks * 50;
 }
 
 const IntervalBase = 1000;
@@ -45,71 +45,71 @@ function onServerStarted() { }
 mc.listen("onServerStarted", onServerStarted);
 
 function refreshItems() {
-	//let start=new Date().getTime()
-	//log("refresh")
-	//为了刷新物品列表，需要将其重置，才能重新向其中添加
-	items = [];
-	mc.getAllEntities().forEach((entity) => {
-		/**遍历发生时的时间 */
-		//let currentTime=new Date().getTime();
-		if (entity.isItemEntity()) {
-			if (!LandEntity(entity)) {
-				//如果当前物品实体找不到uuid就直接中断(已弃用)
-				//if(!entity.uniqueId==null)return;
-				//如果当前物品在白名单中就直接中断
-				if (conf.get("Config").Whitelist.includes(entity.toItem().type)) return;
-				//先检查exist中是否应该清除，再将当前物品的列表加入items（刷新items）
-				if (exist[entity.uniqueId] != undefined) {
-					//在已保存的物品实体列表中发现了当前遍历到的这个实体
-					if (exist[entity.uniqueId] >= getDespawnTime() / IntervalBase) {
-						//实体中保存的留存检测次数达到了实体最大留存检测次数（留存时间/循环间隔），证明这个实体应该被清除
-						//将实体清除
-						entity.despawn();
-						//将实体的相关记录清除
-						//将实体在exist中的记录清除
-						delete exist[entity.uniqueId];
-					} else {
-						//没有超过，那么此次检测就将原记录+1
-						exist[entity.uniqueId]++;
-					}
-				} else {
-					//没有找到这个实体，说明这个实体是第一次出现在插件中，那就把它初始化
-					//将新发现的uuid保存
-					exist[entity.uniqueId] = 1;
-				}
-				//刷新items
-				//items.push(entity);
-				//log(typeof(item.getNbt().getData("UniqueID")))
-			} else {
-				if (conf.get("Config").Whitelist.includes(entity.toItem().type)) return;
-				setTimeout(() => {
-					//将实体清除
-					entity.despawn();
-					//将实体的相关记录清除
-					//将实体在exist中的记录清除
-					delete exist[entity.uniqueId];
-				}, 10000);
-			}
-		}
-	});
-	//let stop=new Date().getTime()
-	//log("耗时"+(stop-start).toString())
+    //let start=new Date().getTime()
+    //log("refresh")
+    //为了刷新物品列表，需要将其重置，才能重新向其中添加
+    items = [];
+    mc.getAllEntities().forEach((entity) => {
+        /**遍历发生时的时间 */
+        //let currentTime=new Date().getTime();
+        if (entity.isItemEntity()) {
+            if (!LandEntity(entity)) {
+                //如果当前物品实体找不到uuid就直接中断(已弃用)
+                //if(!entity.uniqueId==null)return;
+                //如果当前物品在白名单中就直接中断
+                if (conf.get("Config").Whitelist.includes(entity.toItem().type)) return;
+                //先检查exist中是否应该清除，再将当前物品的列表加入items（刷新items）
+                if (exist[entity.uniqueId] != undefined) {
+                    //在已保存的物品实体列表中发现了当前遍历到的这个实体
+                    if (exist[entity.uniqueId] >= getDespawnTime() / IntervalBase) {
+                        //实体中保存的留存检测次数达到了实体最大留存检测次数（留存时间/循环间隔），证明这个实体应该被清除
+                        //将实体清除
+                        entity.despawn();
+                        //将实体的相关记录清除
+                        //将实体在exist中的记录清除
+                        delete exist[entity.uniqueId];
+                    } else {
+                        //没有超过，那么此次检测就将原记录+1
+                        exist[entity.uniqueId]++;
+                    }
+                } else {
+                    //没有找到这个实体，说明这个实体是第一次出现在插件中，那就把它初始化
+                    //将新发现的uuid保存
+                    exist[entity.uniqueId] = 1;
+                }
+                //刷新items
+                //items.push(entity);
+                //log(typeof(item.getNbt().getData("UniqueID")))
+            } else {
+                if (conf.get("Config").Whitelist.includes(entity.toItem().type)) return;
+                setTimeout(() => {
+                    //将实体清除
+                    entity.despawn();
+                    //将实体的相关记录清除
+                    //将实体在exist中的记录清除
+                    delete exist[entity.uniqueId];
+                }, 10000);
+            }
+        }
+    });
+    //let stop=new Date().getTime()
+    //log("耗时"+(stop-start).toString())
 }
 
 /*
 function checkItems(){
-	let currentTime=new Date().getTime();
-	for(let uuid in items){
-		if(items[uuid]+getDespawnTime()<currentTime){
-		    
-		}
-	}
+    let currentTime=new Date().getTime();
+    for(let uuid in items){
+        if(items[uuid]+getDespawnTime()<currentTime){
+            
+        }
+    }
 }
 */
 
 /**创建检测循环 */
 function refreshLoop() {
-	return setInterval(refreshItems, IntervalBase);
+    return setInterval(refreshItems, IntervalBase);
 }
 
 /**
@@ -118,15 +118,15 @@ function refreshLoop() {
  * @returns {number} 实体的UUID
  */
 function getEntityUUID(entity) {
-	if (!entity.getNbt().getKeys().includes("UniqueID")) return null;
-	return entity.getNbt().getData("UniqueID");
+    if (!entity.getNbt().getKeys().includes("UniqueID")) return null;
+    return entity.getNbt().getData("UniqueID");
 }
 
 let mainloop = refreshLoop();
 
 //每半小时清除一次exist防止缓存过大造成安全隐患
 let clearCacheInterval = setInterval(() => {
-	exist = {};
+    exist = {};
 }, 1800000);
 
 /**
@@ -135,14 +135,14 @@ let clearCacheInterval = setInterval(() => {
  * @returns {boolean}
  */
 function LandEntity(Entity) {
-	if (Getland(Entity.pos) == -1) {
-		return false;
-	} else return true;
+    if (Getland(Entity.pos) == -1) {
+        return false;
+    } else return true;
 }
 
 /*
 def test():
-	a=1 
+    a=1 
 
 testmp=multiprocessing.Process(target=test)
 
@@ -156,19 +156,19 @@ testmp.join()
 //测试uuid
 /*
 for item in items:
-	for key in item.getNbt().getKeys():
-		if key!="UniqueID":
-			continue
-		log("掉落物"+item.name+"的nbt中存在UniqueID标签，值为："+str(item.getNbt().getData("UniqueID")))
-		break
+    for key in item.getNbt().getKeys():
+        if key!="UniqueID":
+            continue
+        log("掉落物"+item.name+"的nbt中存在UniqueID标签，值为："+str(item.getNbt().getData("UniqueID")))
+        break
 */
 
 ll.registerPlugin(
-	"DespawnLags",
-	"DespawnLags的脚本引擎版",
-	[0, 0, 1, Version.Dev],
-	{
-		Author: "Tsubasa6848,NewMoonMinecraftStudio",
-		GitHub: "https://www.github.com/231software/DespawnLagsSE",
-	}
+    "DespawnLags",
+    "DespawnLags的脚本引擎版",
+    [0, 0, 1, Version.Dev],
+    {
+        Author: "Tsubasa6848,NewMoonMinecraftStudio",
+        GitHub: "https://www.github.com/231software/DespawnLagsSE",
+    }
 );
